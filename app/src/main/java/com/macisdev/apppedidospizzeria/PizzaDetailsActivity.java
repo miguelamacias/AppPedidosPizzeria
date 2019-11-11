7e6fbcc339c2f6d75f4710bc7e9050a7da7f62d8
Macías <miguel.a.macias.13@gmail.com>
2019-11-11 13:46:12 +0100

    Now the size can be choosen in every pizza.
    //TODO: add the € sign to the price without touching the database.

--- app/src/main/java/com/macisdev/apppedidospizzeria/PizzaDetailsActivity.java
+++ app/src/main/java/com/macisdev/apppedidospizzeria/PizzaDetailsActivity.java
@@ -4,14 +4,20 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 public class PizzaDetailsActivity extends AppCompatActivity {
     public static final String PIZZA_ID = "pizzaId";
+    public static final String MEDIUM_SIZE = "~26cms";
+    public static final String BIG_SIZE = "~40cms";
+
 
     SQLiteDatabase db;
     Cursor cursorNameDetails;
@@ -47,7 +53,7 @@ public class PizzaDetailsActivity extends AppCompatActivity {
 
 
         //we also need the sizes and prices, its easier getting them in a separate query
-        Cursor cursorSizePrice = db.query("pizzas_sizes",
+        final Cursor cursorSizePrice = db.query("pizzas_sizes",
                 new String[]{"_id", "size_id", "price"},
                 "pizza_id = ?",
                 new String[]{String.valueOf(selectedPizzaId)},
@@ -55,13 +61,30 @@ public class PizzaDetailsActivity extends AppCompatActivity {
                 null,
                 null);
 
-        ListView sizes_list = findViewById(R.id.sizes_list);
-        sizes_list.setAdapter(new SimpleCursorAdapter(this,
+        SimpleCursorAdapter adapterSizePrice = new SimpleCursorAdapter(this,
                 android.R.layout.two_line_list_item,
                 cursorSizePrice,
                 new String[]{"size_id", "price"},
                 new int[]{android.R.id.text1, android.R.id.text2},
-                0));
+                0);
+
+        //VewBinder used to format the price correctly
+        adapterSizePrice.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                if (columnIndex == 2) { //price is the column #2 of the cursor
+                    double price = cursor.getDouble(columnIndex); //getting the price as is stored in the db
+                    TextView textView = (TextView) view; //making the generic view a textView
+
+                    textView.setText(String.format(Locale.getDefault(), "%.2f%c", price, '€')); //formatting the price to show the currency sign.
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        ListView sizes_list = findViewById(R.id.sizes_list);
+        sizes_list.setAdapter(adapterSizePrice);
 
 
 
