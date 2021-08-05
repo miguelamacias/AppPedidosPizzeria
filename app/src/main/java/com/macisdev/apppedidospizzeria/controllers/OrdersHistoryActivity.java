5f9eafaf9e09261c658000abb04c3d0c57dc7b4e
Miguel Angel Macias <miguel.a.macias.13@gmail.com>
2021-08-05 16:36:53 +0200

    Version 1.1
    -The history of orders placed is fully implemented.
    -Invoices can be downloaded from the history.

--- app/src/main/java/com/macisdev/apppedidospizzeria/controllers/OrdersHistoryActivity.java
+++ app/src/main/java/com/macisdev/apppedidospizzeria/controllers/OrdersHistoryActivity.java
@@ -1,7 +1,9 @@
 package com.macisdev.apppedidospizzeria.controllers;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -11,7 +13,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.macisdev.apppedidospizzeria.R;
 import com.macisdev.apppedidospizzeria.util.DBHelper;
 
+import java.util.Locale;
+
 public class OrdersHistoryActivity extends AppCompatActivity {
+	SQLiteDatabase db;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +26,7 @@ public class OrdersHistoryActivity extends AppCompatActivity {
 		//create and configure the list
 		ListView ordersHistoryList = findViewById(R.id.orders_history_list);
 
-		SQLiteDatabase db = new DBHelper(this).getReadableDatabase();
+		db = new DBHelper(this).getReadableDatabase();
 		Cursor ordersHistoryCursor = db.rawQuery("SELECT _id, order_id, time FROM orders_history", null);
 
 		SimpleCursorAdapter adapterOrderHistory = new SimpleCursorAdapter(this,
@@ -33,6 +38,19 @@ public class OrdersHistoryActivity extends AppCompatActivity {
 
 		ordersHistoryList.setAdapter(adapterOrderHistory);
 
-		//TODO: Implement click listener to the listview
+		ordersHistoryList.setOnItemClickListener((adapterView, view, i, l) -> {
+			//Cursor to get the orderId of the selected item in the list
+			Cursor selectedOrderCursor = db.rawQuery("SELECT order_id FROM orders_history WHERE _id = ?",
+					new String[]{String.valueOf(l)});
+
+			selectedOrderCursor.moveToFirst();
+			String orderId = selectedOrderCursor.getString(0);
+			selectedOrderCursor.close();
+
+			String url = String.format(Locale.getDefault(), "http://83.47.223.157:8080/invoices/invoice?id=%s",
+					orderId);
+			Intent browser= new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+			startActivity(browser);
+		});
 	}
 }
